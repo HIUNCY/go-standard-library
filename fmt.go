72ecbd58d09465ccda620000de82c3d5d8e0b75b
HIUNCY <zainulkamal393@gmail.com>
2024-03-07 18:30:54 +0700

    fmt / format

---
+++ fmt.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	firstName := "Muhamad"
+	midName := "Zainul"
+	lastName := "Kamal"
+
+	fmt.Printf("Hello, my name is %s %s %s\n", firstName, midName, lastName) // formating string
+}
